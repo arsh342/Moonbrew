552ceeaa47af170f8e731284c6c93ef873a4aac7
arsh342 <arshth134@gmail.com>
2024-12-29 19:47:46 +0530

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
+import { CartProvider } from '../components/CartContext';
+import { AuthProvider } from '../contexts/AuthContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
-export default MyApp; 
+export default MyApp;
