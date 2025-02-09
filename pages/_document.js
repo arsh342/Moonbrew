4d4571627e152ee08055aa72fa8b8de648c024a6
arsh <135307874+arsh342@users.noreply.github.com>
2025-02-09 14:29:59 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         <meta name="theme-color" content="#15803d" />
         <link rel="icon" href="/favicon.ico" />
     <meta name="google-site-verification" content="PZ7tspF0Dsl_d9Zt1_aOoDmIVuEsPV2PJtCERRBzKH8" />
+    <title>Moonbrew Coffee | Coffee Experience</title>  
+<meta name="description" content="Discover Moonbrew, a full-stack coffee website offering premium coffee blends, seamless ordering, and a rich coffee experience. Freshly brewed, just for you!" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://images.unsplash.com" />
       </Head>
