aade2dc4ccf6cc1b0b34c1abc22d763c6d983d4d
arsh <135307874+arsh342@users.noreply.github.com>
2025-02-09 14:16:38 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <meta name="theme-color" content="#15803d" />
         <link rel="icon" href="/favicon.ico" />
+    <meta name="google-site-verification" content="PZ7tspF0Dsl_d9Zt1_aOoDmIVuEsPV2PJtCERRBzKH8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://images.unsplash.com" />
       </Head>
